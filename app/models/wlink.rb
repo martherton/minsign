3b339f171e56d5f0d1f69000d3010f2d27be972e
Mark Artherton <mark.artherton@gmail.com>
2015-01-20 15:43:24 +0000

    csv update enabled

--- app/models/wlink.rb
+++ app/models/wlink.rb
@@ -1,7 +1,7 @@
 class Wlink < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :linkcat
-	validates :wlinkurl, presence: true, uniqueness: true
+	validates :wlinkurl, presence: true
 	validates :wlinkname, presence: true
 	validates :wlinkdesc, presence: true
 	validates :wlinkveri, presence: true
