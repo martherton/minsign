1d8d0474c7e70970a24bfa2efa746c22a56d2e5b
Mark Artherton <mark.artherton@gmail.com>
2015-02-08 21:48:58 +0000

    small add

--- app/controllers/admin/finds_controller.rb
+++ app/controllers/admin/finds_controller.rb
@@ -8,8 +8,8 @@ class Admin::FindsController < ApplicationController
 			if params[:search].present? and params[:search][:q].present?
 				
 				  @finds = Find.search(params[:search][:q])
-				  @users = User.group("email")
-					@linkcats = Linkcat.group("linkcatname") 
+				  @users = User.select("email").group("email") 
+					@linkcats = Linkcat.select("linkcatname").group("linkcatname") 
 					
 					@findscount = Find.count
 					@findslinkcat = Find.group("linkcat_id").count
@@ -24,15 +24,15 @@ class Admin::FindsController < ApplicationController
 					@findsuser = Find.group("user_id").count
 				elsif   params[:search].present? and params[:search][:q2].present? 
 				  @finds = Find.where(:linkcat_id => params[:search][:q2])
-					@users = User.group("email") 
-					@linkcats = Linkcat.group("linkcatname") 
+					@users = User.select("email").group("email") 
+					@linkcats = Linkcat.select("linkcatname").group("linkcatname") 
 					
 					@findscount = Find.count
 					@findslinkcat = Find.group("linkcat_id").count
 					@findsuser = Find.group("user_id").count
 				else	
-					@users = User.group("email")
-					@linkcats = Linkcat.group("linkcatname") 
+					@users = User.select("email").group("email")
+					@linkcats = Linkcat.select("linkcatname").group("linkcatname") 
 					@finds = Find.all
 					@findscount = Find.count
 					@findslinkcat = Find.group("linkcat_id").count
