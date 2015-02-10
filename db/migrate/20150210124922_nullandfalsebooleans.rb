f432ba04eb69ab5e811b29d7f528619f59210488
Mark Artherton <mark.artherton@gmail.com>
2015-02-10 13:30:55 +0000

    Null and false set for booleans and safe entry of booleans

--- db/migrate/20150210124922_nullandfalsebooleans.rb
+++
@@ -1,6 +0,0 @@
-class Nullandfalsebooleans < ActiveRecord::Migration
-  def change
-  	change_column_null :declaratives, :entryend, false, 0
-  	
-  end
-end
--- db/migrate/20150210130538_nullandfalsebooleans2.rb
+++
@@ -1,5 +0,0 @@
-class Nullandfalsebooleans2 < ActiveRecord::Migration
-  def change
-  	change_column_null :declaratives, :sandbox, false, 0
-  end
-end
