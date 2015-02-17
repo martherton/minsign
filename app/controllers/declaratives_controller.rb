59de302ed35b78790ed291f4cf0d59b1094c6075
Mark Artherton <mark.artherton@gmail.com>
2015-02-17 09:47:42 +0000

    friendsandlmits

--- app/controllers/declaratives_controller.rb
+++ app/controllers/declaratives_controller.rb
@@ -251,7 +251,7 @@ class DeclarativesController < ApplicationController
 							
 							
 
-							@declaratives = @decallowed3.where(linkcat_id: :q)
+							@declaratives = @decallowed3.where("linkcat_id = ?", params[:q])
 							
 							
 
